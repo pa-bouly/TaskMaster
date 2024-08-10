208e20cb5783d98b83282b1c9cf542eaa051acfd
P-A Bouly <pierre-alain.bouly@fifty.do>
2024-08-10 11:00:01 +0200

    feat(plugins): add ElementPlus library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useTasksStore } from '@/stores/tasks'
 import TasksList from '@/components/TasksList.vue'
+import { ElButton } from 'element-plus'
+
 const store = useTasksStore()
 
 const onAddTask = () => {
@@ -16,7 +18,7 @@ console.log('store.tasks', store.tasks)
       <h1 class="text-xl font-bold">Tasks</h1>
       <p>{{ store.tasks.length }} tasks</p>
 
-      <button @click="onAddTask">Add task</button>
+      <el-button type="primary" @click="onAddTask">Add task</el-button>
 
       <TasksList :tasks="store.tasks" />
     </div>
