91687a714db2b15a2793791215431b6b9365c69c
P-A Bouly <pierre-alain.bouly@fifty.do>
2024-08-11 09:10:49 +0200

    feat(pinia): add persisted state

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -33,126 +33,136 @@ interface Filter {
   filterByDate?: Date | null
 }
 
-export const useTasksStore = defineStore('tasks', () => {
-  const tasks = ref<Task[]>([])
-  const taskToEdit = ref<Task | null>(null)
-  const filter = ref<Filter>({
-    isCompleted: true,
-    sortBy: undefined,
-    sortDirection: undefined,
-    filterByDate: undefined
-  })
-
-  const completedTasks = computed(() => {
-    return tasks.value.filter((task) => task.isCompleted)
-  })
-
-  const filteredTasks = computed(() => {
-    let copiedTasks = tasks.value.slice()
-    if (!filter.value.isCompleted) {
-      copiedTasks = tasks.value.filter((task) => !task.isCompleted)
-    }
-
-    if (filter.value.filterByDate) {
-      return copiedTasks.filter((task) => {
-        return task.dueDate ? dayjs(task.dueDate).isSame(filter.value.filterByDate, 'day') : false
-      })
-    }
-
-    return copiedTasks
-  })
-
-  const taksFilteredAndSorted = computed(() => {
-    const tasksToSort = filteredTasks.value.slice()
-
-    if (filter.value.sortBy === SortBy.dueDate) {
-      return tasksToSort.sort((a, b) => {
-        if (!a.dueDate) {
-          return -1
-        }
-
-        if (!b.dueDate) {
-          return 1
-        }
-
-        if (filter.value.sortDirection === SortDirection.asc) {
-          return dayjs(a.dueDate).diff(dayjs(b.dueDate))
-        }
-
-        return dayjs(b.dueDate).diff(dayjs(a.dueDate))
-      })
-    }
-
-    if (filter.value.sortBy === SortBy.title) {
-      return tasksToSort.sort((a, b) => {
-        if (filter.value.sortDirection === SortDirection.asc) {
-          return a.title.localeCompare(b.title)
-        }
-
-        return b.title.localeCompare(a.title)
-      })
-    }
-
-    return tasksToSort
-  })
-
-  const isTaskDialogVisible = computed(() => {
-    return taskToEdit.value !== null
-  })
-
-  const addTask = (newTask: TaskToAdd) => {
-    tasks.value.unshift({
-      id: tasks.value.length + 1,
-      isCompleted: false,
-      title: newTask.title,
-      description: newTask.description,
-      dueDate: newTask.dueDate
+export const useTasksStore = defineStore(
+  'tasks',
+  () => {
+    const tasks = ref<Task[]>([])
+    const taskToEdit = ref<Task | null>(null)
+    const filter = ref<Filter>({
+      isCompleted: true,
+      sortBy: undefined,
+      sortDirection: undefined,
+      filterByDate: undefined
     })
+
+    const completedTasks = computed(() => {
+      return tasks.value.filter((task) => task.isCompleted)
+    })
+
+    const filteredTasks = computed(() => {
+      let copiedTasks = tasks.value.slice()
+      if (!filter.value.isCompleted) {
+        copiedTasks = tasks.value.filter((task) => !task.isCompleted)
+      }
+
+      if (filter.value.filterByDate) {
+        return copiedTasks.filter((task) => {
+          return task.dueDate ? dayjs(task.dueDate).isSame(filter.value.filterByDate, 'day') : false
+        })
+      }
+
+      return copiedTasks
+    })
+
+    const taksFilteredAndSorted = computed(() => {
+      const tasksToSort = filteredTasks.value.slice()
+
+      if (filter.value.sortBy === SortBy.dueDate) {
+        return tasksToSort.sort((a, b) => {
+          if (!a.dueDate) {
+            return -1
+          }
+
+          if (!b.dueDate) {
+            return 1
+          }
+
+          if (filter.value.sortDirection === SortDirection.asc) {
+            return dayjs(a.dueDate).diff(dayjs(b.dueDate))
+          }
+
+          return dayjs(b.dueDate).diff(dayjs(a.dueDate))
+        })
+      }
+
+      if (filter.value.sortBy === SortBy.title) {
+        return tasksToSort.sort((a, b) => {
+          if (filter.value.sortDirection === SortDirection.asc) {
+            return a.title.localeCompare(b.title)
+          }
+
+          return b.title.localeCompare(a.title)
+        })
+      }
+
+      return tasksToSort
+    })
+
+    const isTaskDialogVisible = computed(() => {
+      return taskToEdit.value !== null
+    })
+
+    const addTask = (newTask: TaskToAdd) => {
+      tasks.value.unshift({
+        id: tasks.value.length + 1,
+        isCompleted: false,
+        title: newTask.title,
+        description: newTask.description,
+        dueDate: newTask.dueDate
+      })
+    }
+
+    const toggleTask = (id: number) => {
+      const task = tasks.value.find((task) => task.id === id)
+
+      if (!task) return
+
+      task.isCompleted = !task.isCompleted
+    }
+
+    const showTask = (id: number) => {
+      taskToEdit.value = tasks.value.find((task) => task.id === id) || null
+    }
+
+    const hideTask = () => {
+      taskToEdit.value = null
+    }
+
+    const updateTask = (task: Task) => {
+      const index = tasks.value.findIndex((t) => t.id === task.id)
+      if (index === -1) return
+
+      tasks.value.splice(index, 1, task)
+      taskToEdit.value = task
+    }
+
+    const deleteTask = (id: number) => {
+      const index = tasks.value.findIndex((task) => task.id === id)
+      if (index === -1) return
+
+      tasks.value.splice(index, 1)
+    }
+
+    const setFilter = (newFilter: Filter) => {
+      filter.value = newFilter
+    }
+
+    return {
+      tasks,
+      taskToEdit,
+      completedTasks,
+      taksFilteredAndSorted,
+      isTaskDialogVisible,
+      addTask,
+      toggleTask,
+      showTask,
+      hideTask,
+      updateTask,
+      deleteTask,
+      setFilter
+    }
+  },
+  {
+    persist: true
   }
-
-  const toggleTask = (id: number) => {
-    const task = tasks.value.find((task) => task.id === id)
-
-    if (!task) return
-
-    task.isCompleted = !task.isCompleted
-  }
-
-  const showTask = (id: number) => {
-    taskToEdit.value = tasks.value.find((task) => task.id === id) || null
-  }
-
-  const hideTask = () => {
-    taskToEdit.value = null
-  }
-
-  const updateTask = (task: Task) => {
-    const index = tasks.value.findIndex((t) => t.id === task.id)
-    tasks.value.splice(index, 1, task)
-    taskToEdit.value = task
-  }
-
-  const deleteTask = (id: number) => {
-    const index = tasks.value.findIndex((task) => task.id === id)
-    tasks.value.splice(index, 1)
-  }
-
-  const setFilter = (newFilter: Filter) => {
-    filter.value = newFilter
-  }
-
-  return {
-    tasks,
-    taskToEdit,
-    completedTasks,
-    taksFilteredAndSorted,
-    isTaskDialogVisible,
-    addTask,
-    toggleTask,
-    showTask,
-    hideTask,
-    updateTask,
-    deleteTask,
-    setFilter
-  }
-})
+)
